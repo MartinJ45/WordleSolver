1cd05d2343b52c0a8b635cb49cf3b76b32afd1d0
MartinJ45 <bolitaycamilo45@gmail.com>
2025-02-10 19:41:29 -0500

    Handles duplicate letters

--- solve.py
+++ solve.py
@@ -1,30 +1,75 @@
 from fnmatch import fnmatch
+from random import randrange
 
 WORDLE_GUESSES = 5
 
-def updatePossibleAnswers(possibleAnswers, guess, result):
-    yLetters = []
+def getBestGuess(possibleAnswers, guess):
+    bestGuess = ""
+    bestDifference = 0
+    guess_ascii = sum([ord(letter) for letter in guess])
+    for word in possibleAnswers:
+        penalty = 0
+        for i in range(len(word)):
+            w = word
+            letter = word[i]
+            w = w[:i] + w[i+1:]
+            if letter in w:
+                penalty += 10
+
+        word_ascii = sum([ord(letter) for letter in word])
+        difference = abs(guess_ascii - word_ascii) - penalty
+        if difference > bestDifference:
+            bestDifference = difference
+            bestGuess = word
+
+    return bestGuess
+
+def getPossibleAnswers(possibleAnswers, guess, result):
+    yLetters = {}
     nLetters = []
+    gLetters = []
     gToken = "?????"
     yToken = "?????"
+    nToken = "?????"
     for i in range(len(guess)):
         match result[i]:
             case 'G':
+                gLetters.append(guess[i])
                 gToken = gToken[:i] + guess[i] + gToken[i+1:]
             case 'Y':
-                yLetters.append(guess[i])
+                if yLetters.get(guess[i]):
+                    yLetters[guess[i]] += 1
+                else:
+                    yLetters[guess[i]] = 1
                 yToken = yToken[:i] + guess[i] + yToken[i+1:]
             case 'N':
                 nLetters.append(guess[i])
+                nToken = nToken[:i] + guess[i] + nToken[i+1:]
     
     if yToken != "?????":
-        possibleAnswers = [word for word in possibleAnswers if not fnmatch(word, yToken)]
+        for i in range(len(yToken)):
+            token = "?????"
+            if yToken[i] != '?':
+                token = token[:i] + yToken[i] + token[i+1:]
+                possibleAnswers = [word for word in possibleAnswers if not fnmatch(word, token)]
+    
+    if nToken != "?????":
+        possibleAnswers = [word for word in possibleAnswers if not fnmatch(word, nToken)]
     
     newPossibleAnswers = []
+
     for word in possibleAnswers:
-        if all(letter in word for letter in yLetters) and \
-            all(letter not in word for letter in nLetters) and \
-            fnmatch(word, gToken):
+        isIncluded = True
+        for letter in yLetters:
+            if (yLetters[letter] + gLetters.count(letter)) > word.count(letter):
+                isIncluded = False
+        for letter in nLetters:
+            if letter in word and not yLetters.get(letter) and letter not in gLetters:
+                isIncluded = False
+        if not fnmatch(word, gToken):
+            isIncluded = False
+        
+        if isIncluded:
             newPossibleAnswers.append(word)
 
     return newPossibleAnswers
@@ -57,8 +102,12 @@ def main():
                     if result == "GGGGG": 
                         print("You guessed the wordle!")
                         return
-                    possibleAnswers = updatePossibleAnswers(possibleAnswers, guess, result)
-                    print("You should guess:", possibleAnswers[0])
+                    possibleAnswers = getPossibleAnswers(possibleAnswers, guess, result)
+
+                    bestGuess = getBestGuess(possibleAnswers, guess)
+                    if bestGuess == "":
+                        bestGuess = possibleAnswers[randrange(len(possibleAnswers))]
+                    print(f"({len(possibleAnswers)} possible answers) You should guess:", bestGuess)
 
                     invalidInput = False
 
